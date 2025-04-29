2b0eb390c01e738be806450bac7c7ef28b114169
Grimshady1990 <frankiedthomas@gmail.com>
2025-04-29 21:44:35 +0100

    Complete test 1 and initialize test 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,18 @@
 const repeatString = function() {
+    
+    let heyString = "";
+    let i = 0;
+    for (i = 0; i < 3; i++) {
+        heyString = heyString.concat("hey");
+    }
+    return heyString;
 
-};
+    
+       
+} 
+
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
