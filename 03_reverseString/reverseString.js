dab8d406adf63c629b55e0fa4596397307e57d2e
Grimshady1990 <frankiedthomas@gmail.com>
2025-04-30 21:21:48 +0100

    Complete assignment 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let splitWord = word.split("");
+    let revWord = splitWord.reverse();
+    let joinWord = revWord.join("");
+    return joinWord;
 };
 
 // Do not edit below this line
