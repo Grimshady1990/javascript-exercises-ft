bf7144c28e2c5134862585e8759d5509296ca687
Grimshady1990 <frankiedthomas@gmail.com>
2025-04-30 21:52:18 +0100

    Complete test 1  of assignment 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(array, num) {
+
+    const index = array.indexOf(num);
+    if (index > -1) {
+        array.splice(index, 1);
+    }
+    return array;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
