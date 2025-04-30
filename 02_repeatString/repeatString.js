cb137c310a64d023991ad31e076438d9b6c0e5d5
Grimshady1990 <frankiedthomas@gmail.com>
2025-04-30 21:07:27 +0100

    Complete assignment 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,18 +1,24 @@
 const repeatString = function(string, num) {
-
+    let i = 0;
     let globalString = "";
-    
-    for (let i = 0; i < num; i++) {
+    if (num >= 0) {
+    for (i = 0; i < num; i++) {
   
         globalString = globalString.concat(string);
     }
     return globalString;
+    } else {
+        return "ERROR";
+    }
 }
-
+let number = Math.floor(Math.random() * 1000);
 repeatString("hey", 3);
 repeatString("hello", 10);
 repeatString("hi", 2);
 repeatString("bye", 1);
+repeatString("goodbye" -1);
+repeatString("hey", number);
+repeatString("", 10);
 
 
 
