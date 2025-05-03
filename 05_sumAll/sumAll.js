3a3a2eafc9ce22d66f08bb7399ee69aef9e7db83
Grimshady1990 <frankiedthomas@gmail.com>
2025-05-03 05:04:01 +0100

    declare 'i' outside the loop

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,9 +1,11 @@
 const sumAll = function(num1, num2) {
     let total = 0;
-    for (let i = 0; i <= num2; i++) {
-        if (i >= num1) {
+    let i = 0;
+    for (i = 0; i <= num2; i++) {
+        if (i >= num1 && num1 < num2) {
             total += i;
         }
+        
     }
     return total;
 
