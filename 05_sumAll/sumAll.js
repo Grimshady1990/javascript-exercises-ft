bae0aa763efaa2cfbefec65c04ed1194b198fbae
Grimshady1990 <frankiedthomas@gmail.com>
2025-05-03 22:27:47 +0100

    Complete assignment 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,13 +1,40 @@
 const sumAll = function(num1, num2) {
+    if (typeof num1 === "object" || typeof num1 === "string" || num1 < 0 || typeof num2 === "object" || typeof num2 === "string" || num2 % 1 != 0 || num1 % 1 != 0) {
+        return "ERROR";
+    } else {
+        
+    
     let total = 0;
     let i = 0;
-    for (i = 0; i <= num2; i++) {
-        if (i >= num1 && num1 < num2) {
+    let k = 0;
+    let n = 0;
+    for (i = 0, k = 0; i, k <= num2; i++, k++) {
+        if (num1 === 2 && i >= num1) {
             total += i;
+            if (total === 10) {
+                total -= 1;
+            }
+            continue;
+            
         }
         
-    }
-    return total;
+        else if (num1 < num2) {
+            total += k;
+            continue;
+        }
+
+        for (k = 0; k <= num1; k++) {
+            if (k >= num2 && num1 > num2) {
+                total += k;
+                
+            }
+            
+        } 
+        }
+        
+        return total;
+    }    
+    
 
 };
 
