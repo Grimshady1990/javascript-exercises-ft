aeb3441b95e35c21cda54e548cbc1469f2db5258
Grimshady1990 <frankiedthomas@gmail.com>
2025-05-03 04:03:32 +0100

    complete test 1, and test 2

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,11 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let total = 0;
+    for (let i = 0; i <= num2; i++) {
+        if (i >= num1) {
+            total += i;
+        }
+    }
+    return total;
 
 };
 
