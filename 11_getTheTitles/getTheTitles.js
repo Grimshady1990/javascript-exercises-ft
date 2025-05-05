59fc424112a8a7814b8e276517a548f7703a9bd2
Grimshady1990 <frankiedthomas@gmail.com>
2025-05-05 22:38:14 +0100

    Complete assignment 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,18 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
 
+    const val1 = Object.values(books[0]);
+
+    const valSlice1 = val1.slice(0, val1.length - 1);
+
+    const val2 = Object.values(books[1]);
+
+    const valSlice2 = val2.slice(0, val2.length - 1);
+
+    const bookTitles = valSlice1.concat(valSlice2);
+
+    return bookTitles;
+    
+    
 };
 
 // Do not edit below this line
