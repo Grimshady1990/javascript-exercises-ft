92111dab3ea4e86132b13373efe26afe6eaeb15c
Grimshady1990 <frankiedthomas@gmail.com>
2025-05-05 19:33:22 +0100

    Complete assignment 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,32 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+// IF zero or "zero" is equal to number, return zero
+    if (number === 0 || number === "0") return 0;
+
+// IF number is less than zero return "OOPS"
+    if (number < 0) return "OOPS"; 
+
+// IF number is equal too one return one
+    if (number === 1 || number === "1") return 1; 
+
+// IF number is a string convert to number
+    if (typeof number === String) parseInt(number);
+
+// CREATE variables (a) = 1, (b) = 0, (r) = 0,
+    a = 1;
+    b = 0;
+    r = 0;
+
+// FOR i equal to 0, if i is less than number, add one to i
+    for (let i = 1; i < number; i++) {
+
+// LOOP function
+        r = a + b;
+        b = a;
+        a = r;
+    }
+
+    return r;
+
 
 };
 
