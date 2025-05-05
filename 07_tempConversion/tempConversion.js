bbc7cdc03d1a6219bef11a6f10fbf46354f97be4
Grimshady1990 <frankiedthomas@gmail.com>
2025-05-05 06:02:08 +0100

    Complete assignment 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  
+  let temp = (f - 32) * 5 / 9;
+  let rounded = Math.round(temp * 10) / 10;
+
+  return rounded;
+  
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+
+  let temp = (c * 9 / 5) + 32;
+  let rounded = Math.round(temp * 10) / 10;
+  return rounded;
 };
 
 // Do not edit below this line
