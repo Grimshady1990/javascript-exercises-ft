ebdd8a7f0ee656e3e8cc11634e5993a5b1e67f41
Grimshady1990 <frankiedthomas@gmail.com>
2025-05-05 06:28:33 +0100

    Complete assignment 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(a, b) {
+	let result = a + b;
+  return result;
 };
 
 const subtract = function(a, b) {
-	return a + b;
+  let result = a - b;
+	return result;
 };
 
-const sum = function() {
+const sum = function(numbers) {
+  let sumUsingMap = numbers.map(num => num).reduce((acc, curr) => acc + curr, 0);
+	return sumUsingMap;
+};
+
+const multiply = function(numbers) {
+  let sumUsingMap = numbers.map(num => num).reduce((acc, curr) => acc * curr);
+	return sumUsingMap;
+};
+
+const power = function(number, power) {
+  result = number ** power;
+  return result;
 	
 };
 
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(n) {
+	let res = 1;
+  for (let i = 1; i <= n; i++) {
+    res *= i;
+  }
+  return res;
 };
 
 // Do not edit below this line
