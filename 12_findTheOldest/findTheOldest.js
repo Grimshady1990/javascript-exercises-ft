36815ad7fcfba4acc2433f8e1c3e2a8f9a954721
Grimshady1990 <frankiedthomas@gmail.com>
2025-05-07 04:59:37 +0100

    Keep trying to add new key : value to array

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,11 @@
 const findTheOldest = function(people) {
     //let personAge = people.forEach(person => person.yearOfDeath - person.yearOfBirth);
 
+    if (typeof people[0].yearOfDeath === undefined){
+        for (let i )
+    }
+
+
     const oldestPerson = people.reduce((max, person) => 
                                         person.yearOfDeath - person.yearOfBirth > 
                                         max.yearOfDeath - max.yearOfBirth ? 
